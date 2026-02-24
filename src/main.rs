9c6ed5d492df71efa628b7ff3f7ce3d53ca20ae0
plasmaDestroyer <satyasheel2004@gmail.com>
2026-02-24 05:49:44 +0530

    feat: implement pointer, cell, and output operations=

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+
+    let size: usize = 30_000;
+    let mut cells: Vec<u8> = vec![0; size];
+
+    // let input: Vec<char> = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]<.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>+.>++++++++++.".chars().collect();
+    let input: Vec<char> = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.".chars().collect();
+    let input_length: usize = input.len();
+
+    let mut point: usize = 0;
+    let mut input_point: usize = 0;
+    while input_point < input_length {
+        let c: char = input[input_point];
+
+        match c {
+            '>' => point += 1,
+            '<' => point -= 1,
+            '+' => cells[point] = cells[point].wrapping_add(1),
+            '-' => cells[point] = cells[point].wrapping_sub(1),
+            '.' => print!("{}", cells[point] as char),
+            _ => ()
+        }
+
+        input_point += 1
+    }
 }
