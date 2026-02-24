d7bd919f7b1269cf85132e6170bc340b35110f44
plasmaDestroyer <satyasheel2004@gmail.com>
2026-02-24 08:29:03 +0530

    feat: implement loop brackets and output

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     let size: usize = 30_000;
     let mut cells: Vec<u8> = vec![0; size];
 
-    // let input: Vec<char> = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]<.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>+.>++++++++++.".chars().collect();
-    let input: Vec<char> = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.".chars().collect();
+    let input: Vec<char> = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]<.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>+.>++++++++++.".chars().collect();
+    // let input: Vec<char> = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.".chars().collect();
     let input_length: usize = input.len();
 
     let mut point: usize = 0;
@@ -18,9 +18,36 @@ fn main() {
             '+' => cells[point] = cells[point].wrapping_add(1),
             '-' => cells[point] = cells[point].wrapping_sub(1),
             '.' => print!("{}", cells[point] as char),
+            '[' => {
+                if cells[point] == 0 { 
+                    let mut bracket_count: i32 = 1;
+                    while bracket_count > 0 {
+                        input_point += 1;
+                        if input[input_point] == '[' {
+                            bracket_count += 1;
+                        } else if input[input_point] == ']' {
+                            bracket_count -= 1;
+                        }
+                    }
+                }
+            },
+            ']' => {
+                if cells[point] != 0 { 
+                    let mut bracket_count: i32 = 1;
+                    while bracket_count > 0 {
+                        input_point -= 1;
+                        if input[input_point] == ']' {
+                            bracket_count += 1;
+                        } else if input[input_point] == '[' {
+                            bracket_count -= 1;
+                        }
+                    }
+                }
+            },
+
             _ => ()
         }
 
-        input_point += 1
+        input_point += 1;
     }
 }
